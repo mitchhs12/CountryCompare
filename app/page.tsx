cbf1e414d8e9f58d42f617101e40bb84f4f63fd6
valodax <snitzel098@gmail.com>
2024-02-08 20:40:11 -0500

    :white_check_mark: `quicksave` quicksave

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,36 @@
 import Image from "next/image";
 import Link from "next/link";
 import { countries } from "@/utils/data/Countries";
+import Hero from "@/components/Hero";
+
+// Utility function to format country names, with specific handling for certain words
+function formatCountryName(country: string) {
+  const lowercaseWords = ["a", "the", "of"]; // Words to keep in lowercase unless they are the first word
+  return country
+    .split("-")
+    .map((word, index) => {
+      if (index === 0 || !lowercaseWords.includes(word)) {
+        return word.charAt(0).toUpperCase() + word.slice(1);
+      } else {
+        return word;
+      }
+    })
+    .join(" ");
+}
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center">
       <h1 className="flex pt-12 text-2xl">Countries</h1>
-      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 py-12 px-24">
+      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 2xl:grid-cols-7 gap-10 py-12">
         {countries.map((country) => (
           <Link href={`/${country}`} key={country}>
-            <div className="flex flex-col items-center justify-center border-2 gap-y-6">
-              <Image src={`/country-icons/${country}.webp`} alt={country} width={150} height={150} />
-              <span>{country}</span>
+            {/* Use Tailwind CSS classes for responsive container sizes */}
+            <div className="relative group w-40 h-40">
+              <Image src={`/country-icons/${country}.webp`} alt={country} layout="fill" objectFit="cover" />
+              <div className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-20 transition-opacity duration-300 ease-in-out group-hover:bg-opacity-10">
+                <span className=" text-white text-center text-lg font-semibold">{formatCountryName(country)}</span>
+              </div>
             </div>
           </Link>
         ))}
