b1a5ea23a38741740319c266122fa787af87ff01
valodax <snitzel098@gmail.com>
2024-02-10 17:34:37 -0500

    :white_check_mark: `quicksave` quicksave

--- components/Grid/index.tsx
+++ components/Grid/index.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import Image from "next/image";
 import Link from "next/link";
 import GridController from "@/components/GridController";
@@ -7,6 +5,9 @@ import { countries } from "@/utils/data/Countries";
 import { formatCountryName } from "@/utils/helpers";
 
 export default function Grid() {
+  const url = process.env.VERCEL_IMAGE_PREFIX_URL;
+  const compressedUrl = process.env.VERCEL_IMAGE_PREFIX_URL_COMRPESSED;
+  console.log("CURRENT URL", url);
   return (
     <div className="flex-col justify-start">
       <GridController />
@@ -14,7 +15,7 @@ export default function Grid() {
         {countries.map((country, index) => (
           <Link href={`/${country}`} key={country}>
             <div className="relative group">
-              <Image src={`/country-icons/${country}.webp`} alt={country} width={200} height={200} />
+              <Image src={`${compressedUrl}${country}.webp`} alt={country} width={200} height={200} />
               <div className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-20 transition-opacity duration-300 ease-in-out group-hover:bg-opacity-10">
                 <span className="text-white text-center text-lg font-semibold">{formatCountryName(country)}</span>
               </div>
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,21 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "5b9iwcfsiut8ybgm.public.blob.vercel-storage.com",
+        port: "",
+        pathname: "/country-icons/**",
+      },
+      {
+        protocol: "https",
+        hostname: "5b9iwcfsiut8ybgm.public.blob.vercel-storage.com",
+        port: "",
+        pathname: "/country-icons-compressed/**",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
