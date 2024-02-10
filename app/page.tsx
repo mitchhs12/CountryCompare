49bb7fa6b5a0421bcddf1eade5c6177cbebe26fe
valodax <snitzel098@gmail.com>
2024-02-10 15:10:54 -0500

    :rocket: `feat` all images added

--- app/page.tsx
+++ app/page.tsx
@@ -22,11 +22,11 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center">
       <h1 className="flex pt-12 text-2xl">Countries</h1>
-      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 2xl:grid-cols-7 gap-8 py-10">
+      <div className="grid grid-cols-2 sm:grid-cols-4 md:grid-cols-4 lg:grid-cols-6 xl:grid-cols-7 2xl:grid-cols-9 gap-8 py-10 px-10">
         {countries.map((country) => (
           <Link href={`/${country}`} key={country}>
-            <div className="relative group w-40 h-40">
-              <Image src={`/country-icons/${country}.webp`} alt={country} layout="fill" objectFit="cover" />
+            <div className="relative group">
+              <Image src={`/country-icons/${country}.webp`} alt={country} width={200} height={200} />
               <div className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-20 transition-opacity duration-300 ease-in-out group-hover:bg-opacity-10">
                 <span className=" text-white text-center text-lg font-semibold">{formatCountryName(country)}</span>
               </div>
