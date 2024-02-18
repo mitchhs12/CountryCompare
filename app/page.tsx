c7058b1bce9a5fe3977cddbc9404cdf462b24b31
valodax <snitzel098@gmail.com>
2024-02-17 22:54:29 -0500

    :white_check_mark: `quicksave` quicksave

--- app/page.tsx
+++ app/page.tsx
@@ -4,14 +4,14 @@ import Hero from "@/components/Hero";
 import Country from "@/utils/mongodb/models/Country";
 import connect from "@/utils/mongodb/dbConnect";
 
-const getCountryData = cache(async () => {
-  await connect();
-  const countries = await Country.find({});
-  return countries;
-});
+// const getCountryData = cache(async () => {
+//   await connect();
+//   const countries = await Country.find({});
+//   return countries;
+// });
 
 export default async function Home() {
-  const countries = await getCountryData();
+  //const countries = await getCountryData();
   return (
     <main className="flex min-h-screen flex-col items-center justify-center w-full h-full">
       <h1 className="flex pt-12 text-2xl">Countries</h1>
--- components/Globe/LocationGlobe/index.tsx
+++ components/Globe/LocationGlobe/index.tsx
@@ -7,77 +7,71 @@ import { hexToRGBA } from "@/utils/helpers";
 const Globe = dynamic(() => import("react-globe.gl"), { ssr: false });
 
 export default function LocationGlobe() {
-  const globeEl = useRef<any>(null);
+  const globeEl = useRef<any>();
   const [hoverD, setHoverD] = useState(null);
-  const countries = useMemo(() => countryData, []);
+  const [isGlobeLoaded, setIsGlobeLoaded] = useState(false);
 
   useEffect(() => {
-    if (globeEl.current) {
-      globeEl.current.controls().autoRotate = true;
-      globeEl.current.controls().autoRotateSpeed = 0.5;
-    }
-  }, [globeEl]);
-
-  const getVal = (feat: any) => {
-    return Math.max(1e5, Number(feat.properties.POP_EST));
-  };
-
-  const maxVal = useMemo(() => Math.max(...countries.features.map(getVal)), [countries]);
+    globeEl.current.controls().autoRotate = true;
+    globeEl.current.controls().autoRotateSpeed = 0.1;
+  }, []);
 
   // Wrapper function to match expected signature
   const handlePolygonHover = (polygon: any | null, prevPolygon: any | null) => {
     setHoverD(polygon);
   };
 
-  const colorScale = d3.scaleSequentialSqrt(d3.interpolateYlOrRd).domain([0, maxVal]);
-
   return (
     <div>
-      <Globe
-        ref={globeEl}
-        animateIn={false}
-        globeImageUrl="//unpkg.com/three-globe/example/img/earth-blue-marble.jpg"
-        //bumpImageUrl="//unpkg.com/three-globe/example/img/earth-topology.png"
-        lineHoverPrecision={0}
-        width={500}
-        height={500}
-        backgroundColor={hexToRGBA("#ffffff", 0)}
-        polygonsData={countryData.features.filter((d) => d.properties.ISO_A2 !== "AQ")}
-        polygonAltitude={({ properties }: any) => (properties.ISO_A2 === "DZ" ? 0.12 : 0.01)}
-        polygonCapColor={(d: any) =>
-          d == hoverD
-            ? d.properties.ISO_A2 === "DZ"
-              ? "rgba(39, 174, 96, 0.7)"
-              : "rgba(255, 255, 255, 0.5)"
-            : d.properties.ISO_A2 === "DZ"
-            ? "rgba(70, 130, 180, 0.7)"
-            : "rgba(255, 255, 255, 0.1)"
-        }
-        polygonSideColor={(d: any) =>
-          d == hoverD
-            ? d.properties.ISO_A2 === "DZ"
-              ? "rgba(39, 174, 96, 0.7)"
-              : "rgba(0, 0, 0, 0.1)"
-            : d.properties.ISO_A2 === "DZ"
-            ? "rgba(70, 130, 180, 0.7)"
-            : "rgba(0, 0, 0, 0.1)"
-        }
-        polygonStrokeColor={(d: any) =>
-          d == hoverD
-            ? d.properties.ISO_A2 === "DZ"
-              ? "rgba(39,174,96,0.9)"
-              : "rgba(255, 255, 255, 0.1)"
-            : d.properties.ISO_A2 === "DZ"
-            ? "rgba(70, 130, 180, 0.9)"
-            : "rgba(0, 0, 0, 0.1)"
-        }
-        polygonLabel={({ properties }: any) => `
+      {isGlobeLoaded && (
+        <div>
+          <Globe
+            ref={globeEl}
+            animateIn={false}
+            globeImageUrl="//unpkg.com/three-globe/example/img/earth-blue-marble.jpg"
+            //bumpImageUrl="//unpkg.com/three-globe/example/img/earth-topology.png"
+            lineHoverPrecision={0}
+            width={500}
+            height={500}
+            backgroundColor={hexToRGBA("#ffffff", 0)}
+            polygonsData={countryData.features.filter((d) => d.properties.ISO_A2 !== "AQ")}
+            polygonAltitude={({ properties }: any) => (properties.ISO_A2 === "DZ" ? 0.12 : 0.01)}
+            polygonCapColor={(d: any) =>
+              d == hoverD
+                ? d.properties.ISO_A2 === "DZ"
+                  ? "rgba(39, 174, 96, 0.7)"
+                  : "rgba(255, 255, 255, 0.5)"
+                : d.properties.ISO_A2 === "DZ"
+                ? "rgba(70, 130, 180, 0.7)"
+                : "rgba(255, 255, 255, 0.1)"
+            }
+            polygonSideColor={(d: any) =>
+              d == hoverD
+                ? d.properties.ISO_A2 === "DZ"
+                  ? "rgba(39, 174, 96, 0.7)"
+                  : "rgba(0, 0, 0, 0.1)"
+                : d.properties.ISO_A2 === "DZ"
+                ? "rgba(70, 130, 180, 0.7)"
+                : "rgba(0, 0, 0, 0.1)"
+            }
+            polygonStrokeColor={(d: any) =>
+              d == hoverD
+                ? d.properties.ISO_A2 === "DZ"
+                  ? "rgba(39,174,96,0.9)"
+                  : "rgba(255, 255, 255, 0.1)"
+                : d.properties.ISO_A2 === "DZ"
+                ? "rgba(70, 130, 180, 0.9)"
+                : "rgba(0, 0, 0, 0.1)"
+            }
+            polygonLabel={({ properties }: any) => `
           <b>${properties.ADMIN} (${properties.ISO_A2}):</b> <br />
           Population: <i>${properties.POP_EST}</i>
         `}
-        onPolygonHover={handlePolygonHover}
-        polygonsTransitionDuration={300}
-      />
+            onPolygonHover={handlePolygonHover}
+            polygonsTransitionDuration={300}
+          />
+        </div>
+      )}
     </div>
   );
 }
