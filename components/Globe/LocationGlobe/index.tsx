8dd085b706254af4f895d20ba2fdcd8b0dc5ccaa
valodax <snitzel098@gmail.com>
2024-02-19 23:02:01 -0500

    :white_check_mark: `quicksave` auto rotate fixed

--- components/Globe/LocationGlobe/index.tsx
+++ components/Globe/LocationGlobe/index.tsx
@@ -5,11 +5,13 @@ import Globe from "react-globe.gl";
 import Loading from "@/components/Loading";
 import { countries } from "@/utils/data/Countries";
 import { usePathname } from "next/navigation";
-import { checkForAllCountries } from "@/utils/helpers";
+import { FeatureCollection } from "geojson";
 import * as THREE from "three";
+import * as turf from "@turf/turf";
 
 export default function LocationGlobe() {
   const globeEl = useRef<any>(null);
+
   const cloudsMeshRef = useRef<THREE.Mesh>();
   const shouldRotateCloudsRef = useRef(true); // Ref to control clouds rotation
 
@@ -17,35 +19,53 @@ export default function LocationGlobe() {
   const [isLoading, setIsLoading] = useState(true);
   const countryId = usePathname().split("/")[1];
   const [theme, setTheme] = useState("light");
+  const [isSpinning, setIsSpinning] = useState(false);
 
   // Add clouds sphere
   const CLOUDS_IMG_URL = "./clouds.png";
   const CLOUDS_ALT = 0.004;
   const CLOUDS_ROTATION_SPEED = -0.006; // deg/frame
 
-  useEffect(() => {
-    checkForAllCountries();
-  }, []);
+  const globeImageUrl = theme === "dark" ? "./blue-marble.jpg" : "./day.jpg";
+  const typedAllData = allData as FeatureCollection;
+  const isoCode = countries[countryId as keyof typeof countries].isoCode;
 
   useEffect(() => {
+    // Function to update the theme based on the "dark" class in the document element
     const updateTheme = () => {
       const isDarkMode = document.documentElement.classList.contains("dark");
       setTheme(isDarkMode ? "dark" : "light");
     };
 
+    // Function to handle visibility change
+    const handleVisibilityChange = () => {
+      if (document.hidden) {
+        globeEl.current?.pauseAnimation(); // Pause the globe animation when the document is hidden
+      } else {
+        globeEl.current?.resumeAnimation(); // Resume the globe animation when the document becomes visible again
+      }
+    };
+
+    // Initial theme update and setting up an observer for class changes on the document element
     updateTheme();
     const observer = new MutationObserver(updateTheme);
     observer.observe(document.documentElement, { attributes: true, attributeFilter: ["class"] });
 
-    return () => observer.disconnect();
-  }, []);
+    // Adding event listener for visibility change
+    document.addEventListener("visibilitychange", handleVisibilityChange);
+
+    // Cleanup function to remove the event listener and disconnect the observer
+    return () => {
+      document.removeEventListener("visibilitychange", handleVisibilityChange);
+      observer.disconnect();
+    };
+  }, []); // Empty dependency array ensures this effect runs only once on mount
 
   const handleGlobeReady = () => {
     const globe = globeEl.current;
+
     setIsLoading(false);
     const controls = globe.controls();
-    controls.autoRotate = true;
-    controls.autoRotateSpeed = 1;
 
     new THREE.TextureLoader().load(CLOUDS_IMG_URL, (cloudsTexture) => {
       const clouds = new THREE.Mesh(
@@ -61,8 +81,27 @@ export default function LocationGlobe() {
           requestAnimationFrame(rotateClouds);
         }
       };
+
+      const countryFeatures = typedAllData.features.filter((d: any) => d.properties.ISO_A2_EH === isoCode);
+
+      // Assuming countryFeatures is an array of GeoJSON Feature objects for the country
+      const featureCollection = turf.featureCollection(countryFeatures);
+      const centroid = turf.centroid(featureCollection);
+
+      const coordinates = centroid.geometry.coordinates;
+
+      globe.pointOfView({ lat: coordinates[1], lng: coordinates[0], altitude: 1.7 }, 1000);
       rotateClouds();
     });
+  };
+
+  const startStopSpinning = () => {
+    if (isSpinning) {
+      stopSpinning();
+    } else {
+      startSpinning();
+    }
+    setIsSpinning(!isSpinning);
   };
 
   // Wrapper function to match expected signature
@@ -73,30 +112,26 @@ export default function LocationGlobe() {
   const stopSpinning = () => {
     const controls = globeEl.current.controls();
     controls.autoRotate = false;
-    shouldRotateCloudsRef.current = false;
+    //shouldRotateCloudsRef.current = false;
   };
 
   const startSpinning = () => {
     const controls = globeEl.current.controls();
     controls.autoRotate = true;
-    shouldRotateCloudsRef.current = true;
-    if (cloudsMeshRef.current) {
-      const rotateClouds = () => {
-        if (shouldRotateCloudsRef.current && cloudsMeshRef.current) {
-          cloudsMeshRef.current.rotation.y += (CLOUDS_ROTATION_SPEED * Math.PI) / 180; // Ensure this matches the initial speed
-          requestAnimationFrame(rotateClouds);
-        }
-      };
-      rotateClouds();
-    }
+    // shouldRotateCloudsRef.current = true;
+    // if (cloudsMeshRef.current) {
+    //   const rotateClouds = () => {
+    //     if (shouldRotateCloudsRef.current && cloudsMeshRef.current) {
+    //       cloudsMeshRef.current.rotation.y += (CLOUDS_ROTATION_SPEED * Math.PI) / 180; // Ensure this matches the initial speed
+    //       requestAnimationFrame(rotateClouds);
+    //     }
+    //   };
+    //   rotateClouds();
+    // }
   };
 
-  const globeImageUrl = theme === "dark" ? "./blue-marble.jpg" : "./day.jpg";
-
-  const isoCode = countries[countryId as keyof typeof countries].isoCode;
-
   return (
-    <div className="relative w-[500px] h-[500px]" onMouseEnter={stopSpinning} onMouseLeave={startSpinning}>
+    <div className="relative w-[500px] h-[500px]">
       {isLoading && (
         <div className="absolute inset-0 flex justify-center items-center">
           <Loading size={100} />
@@ -106,15 +141,13 @@ export default function LocationGlobe() {
         ref={globeEl}
         onGlobeReady={handleGlobeReady}
         waitForGlobeReady={true}
-        animateIn={true}
+        animateIn={false}
         globeImageUrl={globeImageUrl} //"//unpkg.com/three-globe/example/img/earth-blue-marble.jpg"
         bumpImageUrl="//unpkg.com/three-globe/example/img/earth-topology.png"
         width={500}
         height={500}
         backgroundColor={hexToRGBA("#ffffff", 0)}
-        polygonsData={allData.features.filter(
-          (d) => d.properties.ISO_A2_EH === isoCode
-        )}
+        polygonsData={typedAllData.features.filter((d: any) => d.properties.ISO_A2_EH === isoCode)}
         polygonAltitude={({ properties }: any) => (properties.ISO_A2_EH === isoCode ? 0.15 : 0.01)}
         polygonCapColor={(d: any) =>
           d == hoverD
@@ -122,7 +155,7 @@ export default function LocationGlobe() {
               ? "rgba(39, 174, 96, 0.7)"
               : "rgba(255, 255, 0, 0.5)"
             : d.properties.ISO_A2_EH === isoCode
-            ? "rgba(70, 130, 180, 0.7)"
+            ? "rgba(255, 255, 0, 0.7)"
             : "rgba(255, 255, 255, 0.1)"
         }
         polygonSideColor={(d: any) =>
@@ -131,7 +164,7 @@ export default function LocationGlobe() {
               ? "rgba(39, 174, 96, 0.7)"
               : "rgba(0, 0, 0, 0.1)"
             : d.properties.ISO_A2_EH === isoCode
-            ? "rgba(70, 130, 180, 0.7)"
+            ? "rgba(255, 255, 0, 0.7)"
             : "rgba(0, 0, 0, 0.1)"
         }
         polygonStrokeColor={(d: any) =>
@@ -140,15 +173,18 @@ export default function LocationGlobe() {
               ? "rgba(39,174,96,0.9)"
               : "rgba(255, 255, 255, 0.1)"
             : d.properties.ISO_A2_EH === isoCode
-            ? "rgba(70, 130, 180, 0.9)"
+            ? "rgba(255, 255, 0, 0.9)"
             : "rgba(0, 0, 0, 0.1)"
         }
         polygonLabel={({ properties }: any) => `
           <b>${properties.ADMIN} (${properties.ISO_A2_EH}):</b> <br />
-          Population: <i>${properties.POP_EST.toLocaleString()}</i>
+          Population: <i>${properties.POP_EST.toLocaleString()} (${properties.POP_YEAR})</i>
         `}
         onPolygonHover={handlePolygonHover}
         polygonsTransitionDuration={300}
+        onGlobeClick={() => {
+          startStopSpinning();
+        }}
       />
     </div>
   );
